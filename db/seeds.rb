fb91d773330e437efaab9bb8d6bdbaa1c871b3b1
JoA Azria <joa.azria@gmail.com>
2020-10-15 15:01:23 -0300

    challenge done

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,15 @@
 
 puts "Cleaning database..."
 Restaurant.destroy_all
-
-puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "140421", category: "french" }
-pizza_east =  { name: "Pizza", address: "9  St, Itali E2 7JE", phone_number: "14022", category: "italian" }
-jap =  { name: "Jap", address: "9  St, TOKYO E2 7JE", phone_number: "333333", category: "japanese" }
-chin =  { name: "china", address: "9 hajka", phone_number: "3334432", category: "chinese" }
-be =  { name: "lagouloute", address: "mankanpee", phone_number: "00000", category: "belgian" }
-
-
-
-[ dishoom, pizza_east, jap, chin, be ].each do |attributes|
-  restaurant = Restaurant.create!(attributes)
-  puts "Created #{restaurant.name}"
+puts "Creating Fake Restaurants"
+100.times do |i|
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+  puts "Finished! restaurant #{ i + 1} created"
 end
-puts "Finished!"
+
+
